615acc6fd068f413a3ed62ebf477d62611749c44
mohmedvaid <hussainvaid7@gmail.com>
2025-03-03 17:17:25 -0600

    setup next.js and tailwind

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Tailwind is working! 🎉</h1>
+    </div>
+  );
+}
+
